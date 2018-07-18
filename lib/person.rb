dd075819256a1910aa320e65622a3b8b7097ac2f
yendai-vo <yendai.vo@hotmail.com>
2018-07-18 18:20:18 -0500

    finished 00 project

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,94 @@
-# your code goes here
+require 'pry'
+
+class Person
+
+  attr_accessor :hygiene, :bank_account
+  # attr_reader :bank_account
+
+
+
+  def initialize(initial_balance = 25, initial_happiness = 8, initial_hygiene = 8, name)
+
+    @bank_account = initial_balance
+    @happiness = initial_happiness
+    @hygiene = initial_hygiene
+    @name = name
+
+  end
+
+  def name
+    @name
+  end
+
+  def happiness
+    @happiness
+  end
+
+  def happiness=(new_happiness)
+    @happiness = new_happiness
+    # if @happiness >= 0 && @happiness <= 10
+    #   new_happiness
+    # elsif @happiness > 10
+    #   new_happiness = 10
+    # else
+    #   new_happiness = 0
+    # end
+    @happiness = 0 if @happiness < 0
+    @happiness = 10 if @happiness > 10
+  end
+
+  def hygiene=(new_hygiene)
+    @hygiene = new_hygiene
+    @hygiene = 0 if @hygiene < 0
+    @hygiene = 10 if @hygiene > 10
+  end
+  def clean?
+    if @hygiene > 7
+      true
+    else false
+    end
+  end
+
+  def happy?
+    if @happiness > 7
+      true
+    else false
+    end
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    return "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -=3
+    self.happiness +=2
+    return "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness +=3
+    friend.happiness +=3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(name, topic)
+    if topic == "politics"
+      self.happiness -=2
+      name.happiness -=2
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness +=1
+      name.happiness +=1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+end
